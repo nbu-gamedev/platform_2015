25555dd57861cad9ca244c56e632ace3129b9e5d
samir-github <s.muradov88@gmail.com>
2015-11-08 17:28:20 +0200

    variable name changes

--- Platform/World.h
+++ Platform/World.h
@@ -7,10 +7,9 @@ class World
 public:
 	static const int SCREEN_WIDTH = 1280;
 	static const int SCREEN_HEIGHT = 720;
-	static const int PIXEL_PER_GRID = 20;
-	static const int GRID_WIDTH = SCREEN_WIDTH / PIXEL_PER_GRID;
-	static const int GRID_HEIGHT = SCREEN_HEIGHT / PIXEL_PER_GRID;
+	static const int PIXEL_PER_TILE = 20;
+	static const int GRID_WIDTH = SCREEN_WIDTH / PIXEL_PER_TILE;
+	static const int GRID_HEIGHT = SCREEN_HEIGHT / PIXEL_PER_TILE;
 	Actor* worldGrid[GRID_HEIGHT][GRID_WIDTH] = {};
-	
 
 };
