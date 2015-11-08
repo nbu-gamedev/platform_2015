7c6abe35393eec57703fb957234e429dedfee799
samir-github <s.muradov88@gmail.com>
2015-11-08 10:26:22 +0200

    The grid for the world class.
    This is uploaded so the other members can start using the grid to
    update() and draw() objects.

--- Platform/World.h
+++ Platform/World.h
@@ -4,8 +4,13 @@
 
 class World
 {
-
-
-
+public:
+	static const int SCREEN_WIDTH = 1280;
+	static const int SCREEN_HEIGHT = 720;
+	static const int PIXEL_PER_GRID = 20;
+	static const int GRID_WIDTH = SCREEN_WIDTH / PIXEL_PER_GRID;
+	static const int GRID_HEIGHT = SCREEN_HEIGHT / PIXEL_PER_GRID;
+	Actor* worldGrid[GRID_HEIGHT][GRID_WIDTH] = {};
+	
 
 };
