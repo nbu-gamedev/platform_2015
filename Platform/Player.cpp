115a6b1690d34aa6188737e0becf5acad3ff864c
karonka <karonka@abv.bg>
2015-11-13 20:42:01 +0200

    bug while calucating position fixed

--- Platform/Player.cpp
+++ Platform/Player.cpp
@@ -5,19 +5,23 @@
 Player::Player(SDL_Rect pos/*, SDL_Rect img*/)
 {
     pos_rect = pos;
+    real_x = pos.x;
+    real_y = pos.y;
     //img_rect = img;
     speed = 0;
+    jump_height = max_jump_height/2;
     moving = false;
     h_direction = 1;
     v_direction = 0;
 }
 void Player::move_player(int time)
 {
-    int moved = 0;
+    float moved = 0;
     if (moving)
     {
-        moved = std::min(time*(speed +(acceleration*time/2)/1000)/1000, max_speed_player*time/1000);
-        pos_rect.x += (h_direction)*moved;
+        moved = std::min(time*(speed +(acceleration*time/2.)/1000.)/1000., max_speed_player*time/1000.);
+        real_x += (h_direction)*moved;
+        pos_rect.x = real_x;
         if ( pos_rect.x > 1280 - pos_rect.w) // 1280 == screen_width
             pos_rect.x = 1280 - pos_rect.w;
         else if( pos_rect.x < 0)
@@ -34,44 +38,66 @@ void Player::move_player(int time)
         else if( pos_rect.x < 0)
             pos_rect.x = 0;
         speed = std::max(speed - acceleration*time / 1000, 0);
-    }*/
+    }
     //plavno spirane
-    else if (!moving && speed > 0)
+    else if (!moving && !v_direction && speed > 0)
     {
 
     }
+    //jumpig
+    else if(!moving && v_direction)
+    {
+
+    }*/
     else
     {
         speed = 0;
     }
+
+}
+void Player::check_collisions()//(grid*)
+{
+    int x_grid = getGridCoords().first;
+    int y_grid = getGridCoords().second;
+
+    // check for platform
+    /*
+
+    */
+    // check for enemy
+    /*
+
+    */
+    // check for coins
+    /*
+
+    */
 }
 
 void Player::update(int time_passed, Key key, Type key_type)
 {
-    if (key == UP || key == LEFT || key == RIGHT)
+    if (key == LEFT || key == RIGHT)
     {
         if(key_type == PRESSED)
         {
             moving = true;
-            switch(key)
-            {
-                case UP:
-                v_direction = 1;
-                moving = false;
-                break;
-                case LEFT:
-                h_direction = -1;
-               // v_direction = 0;
-                break;
-                case RIGHT:
-                h_direction = 1;
-               // v_direction = 0;
-                break;
-                default: {}
-            }
+            h_direction = key == LEFT ? -1 : 1;
         }
         else
             moving  = false;
         }
-        move_player(time_passed);
+     if (key == UP)
+     {
+       //jump
+     }
+
+    move_player(time_passed);
+    check_collisions();
+
+}
+std::pair<int,int> Player::getGridCoords()
+{
+    int x_grid = pos_rect.x / 70;// PixelPerTile;
+    int y_grid = pos_rect.y / 70; //PixelPerTile;
+    return std::make_pair(x_grid,y_grid);
 }
--- Platform/Player.h
+++ Platform/Player.h
@@ -3,7 +3,9 @@
 #include "Input_Handler.h"
 
 const int max_speed_player = 180; // TO BE CHANGED
-const int acceleration = 80; // TO BE CHANGED
+const int max_jump_height = 430; // TO BE CHANGED
+
+const int acceleration = 400; // TO BE CHANGED
 
 class Player:public Actor
 {
@@ -13,8 +15,12 @@ public:
 
     //void render();
     void move_player(int time);
-    // void check_collisions();
+    void check_collisions();
+    std::pair<int,int> getGridCoords();
+
+    float real_x,real_y;
     int h_direction;
     int v_direction;
     bool moving;
+    int jump_height;
 };
