4469be8e9bedc3a7acc31f2f9b1f42d249866c26
qvkaa <yavor.aleksiev07@gmail.com>
2015-12-12 16:37:41 +0200

    Coin animation implemented
    Coin animation implemented

--- Platform/Coin.cpp
+++ Platform/Coin.cpp
@@ -16,8 +16,39 @@ bool Coin::update(int time_passed, Key_event* ke)
 
 void Coin::render(SDL_Renderer * renderer, int time_passed, CoreEngine & core)
 {
+	/*
+	static SDL_RendererFlip flip = SDL_FLIP_VERTICAL;
+	static int lastUpdated = 0;
+	static int frame = 0;
+	lastUpdated += time_passed;
+	if (lastUpdated > 1000)
+	{
+		frame++;
+		frame %= core.enemy_textures.size();
+		lastUpdated -= 1000;
+	}
+	
+		if (direction < 0)
+		{
+			SDL_RenderCopy(renderer, core.enemy_textures[frame], NULL, &pos_rect);
+			
+		}
+		else
+		{
+			SDL_RenderCopyEx(renderer, core.enemy_textures[frame], NULL, &pos_rect, 180.0f, NULL, flip);
+		}
+	*/
+	static int lastUpdated = 0;
+	static int frame = 0;
+	lastUpdated += time_passed;
+	if (lastUpdated > 4200)
+	{
+		frame++;
+		frame %= core.coin_textures.size();
+		lastUpdated -= 4200;
+	}
 	if (!taken)
 	{
-        SDL_RenderCopy(renderer, core.coin_textures[2], NULL, &pos_rect);
+        SDL_RenderCopy(renderer, core.coin_textures[frame], NULL, &pos_rect);
     }
 }
