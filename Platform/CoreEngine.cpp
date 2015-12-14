0c7c8954fdb48c1ef6024f922bfdd9a34e89b2c6
samir-github <Samir>
2015-12-14 20:29:46 +0200

    Changed to init everything

--- Platform/CoreEngine.cpp
+++ Platform/CoreEngine.cpp
@@ -17,8 +17,9 @@ bool CoreEngine :: init()
 	//Initialization flag
 	bool success = true;
 
+
 	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
 		success = false;
